6d604cc32662948ae0638285a95d5b657792dac7
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2021-10-28 11:25:48 +0200

    Clean up visualizer

--- AircraftVisualizer.m
+++ AircraftVisualizer.m
@@ -1,24 +1,21 @@
 classdef AircraftVisualizer
     properties
         Model3D
-        TextHandles = {};
-        aircraft_transformation
-        aircraft_dimensions
-        ax_3d
-        ax_inputs = {};
-        ax_pos
-        fig
-        timestep = 0.02
-        InputPlotHandles = {};
-        PosPlotHandle;
+        Animation
     end
     
     methods
         function obj = AircraftVisualizer()
+            obj.Animation.timestep = 0.02;
+            obj.Animation.scaling = 1.0;
+            
+            % Set this to true to show live plot of position
+            obj.Animation.show_position_plot = false;
+            
             obj.Model3D.cg_position_from_front = -0.494;
             obj.Model3D.cg_position_from_bottom = 0.25;
             obj.Model3D.wingspan = 2.5;
-            obj.Model3D.alpha = 0.8;
+            obj.Model3D.alpha = 1.0;
             obj.Model3D.color = [0.8 0.8 1.0];
             
             model = stlread('3d_files/babyshark.stl');
@@ -26,10 +23,11 @@ classdef AircraftVisualizer
             obj.Model3D.stl_data.faces = model.faces;
             obj = obj.initialize_aircraft_model();
             
-            obj.aircraft_dimensions = max(max(sqrt(sum(obj.Model3D.stl_data.vertices.^2, 2))));
+            obj.Model3D.aircraft_dimensions = max(max(sqrt(sum(obj.Model3D.stl_data.vertices.^2, 2))));
             obj = obj.create_ax_object();
-            obj.aircraft_transformation = hgtransform('Parent', obj.ax_3d); % Transformation object that is used to rotate the aircraft            
+            obj.Animation.aircraft_transformation = hgtransform('Parent', obj.Animation.PlotAxes.ax_3d); % Transformation object that is used to rotate the aircraft            
         end
+        
         function obj = initialize_aircraft_model(obj)
             % Import an STL mesh, returning a PATCH-compatible face-vertex structure
             V0 = obj.Model3D.stl_data.vertices;
@@ -89,25 +87,25 @@ classdef AircraftVisualizer
         end
         
         function render_plot(obj)
-            axis(obj.ax_3d, 'equal');
-            viewbox = [-1 1 -1 1 -1 1] * 2 * obj.aircraft_dimensions;
-            axis(obj.ax_3d, viewbox);
+            axis(obj.Animation.PlotAxes.ax_3d, 'equal');
+            viewbox = [-1 1 -1 1 -1 1] * (1/obj.Animation.scaling) * obj.Model3D.aircraft_dimensions;
+            axis(obj.Animation.PlotAxes.ax_3d, viewbox);
             set(gcf,'Color',[1 1 1])
-            view(obj.ax_3d, [30 10])
-            camlight(obj.ax_3d, 'left');
-            material(obj.ax_3d, 'dull');
+            view(obj.Animation.PlotAxes.ax_3d, [30 10])
+            camlight(obj.Animation.PlotAxes.ax_3d, 'left');
+            material(obj.Animation.PlotAxes.ax_3d, 'dull');
         end
         
         function plot_aircraft(obj)
-            patch(obj.ax_3d, 'Faces', obj.Model3D.stl_data.faces, ...
+            patch(obj.Animation.PlotAxes.ax_3d, 'Faces', obj.Model3D.stl_data.faces, ...
                 'Vertices', obj.Model3D.stl_data.vertices, ...
                  'FaceColor', obj.Model3D.color, ...
                  'FaceAlpha', obj.Model3D.alpha, ...
                  'EdgeColor',       'none',        ...
                  'FaceLighting',    'gouraud',     ...
                  'AmbientStrength', 0.15,...
-                 'Parent', obj.aircraft_transformation); hold on
-             scatter3(obj.ax_3d, 0,0,0,'filled');
+                 'Parent', obj.Animation.aircraft_transformation); hold on
+             scatter3(obj.Animation.PlotAxes.ax_3d, 0,0,0,'filled');
              obj.render_plot();
         end
         
@@ -117,48 +115,48 @@ classdef AircraftVisualizer
             ylabels = ["deg [^circ]" "deg [^circ]" "deg [^circ]"];
             
             for i = 1:3
-                obj.InputPlotHandles{i} = plot(obj.ax_inputs{i}, t(1), rad2deg(x(1)), 'XDataSource', 'time', 'YDataSource', y_datasources(i));
-                xlim(obj.ax_inputs{i}, [t(1), t(end)]);
-                ylim(obj.ax_inputs{i}, [-25, 25]);
+                obj.Animation.InputPlotHandles{i} = plot(obj.Animation.PlotAxes.ax_inputs{i}, t(1), rad2deg(x(1)), 'XDataSource', 'time', 'YDataSource', y_datasources(i));
+                xlim(obj.Animation.PlotAxes.ax_inputs{i}, [t(1), t(end)]);
+                ylim(obj.Animation.PlotAxes.ax_inputs{i}, [-25, 25]);
                 
-                title(obj.ax_inputs{i}, input_plot_names(i));
-                ylabel(obj.ax_inputs{i}, ylabels(i));
-                grid(obj.ax_inputs{i}, 'on'); 
-                box(obj.ax_inputs{i}, 'on');
+                title(obj.Animation.PlotAxes.ax_inputs{i}, input_plot_names(i));
+                ylabel(obj.Animation.PlotAxes.ax_inputs{i}, ylabels(i));
+                grid(obj.Animation.PlotAxes.ax_inputs{i}, 'on'); 
+                box(obj.Animation.PlotAxes.ax_inputs{i}, 'on');
             end
             
-            xlabel(obj.ax_inputs{3}, "Time [s]");
+            xlabel(obj.Animation.PlotAxes.ax_inputs{3}, "Time [s]");
         end
         
         function [t, x] = prepare_trajectory(obj, t_trajectory, x_trajectory)
             % Make sure dt is constant for trajectory
             t_0 = t_trajectory(1);
             t_end = t_trajectory(end);
-            t = t_0:obj.timestep:t_end;
+            t = t_0:obj.Animation.timestep:t_end;
             x = interp1(t_trajectory,x_trajectory,t); 
         end
         
         function obj = plot_text(obj)
-            obj.TextHandles.time_text_hdl = text(obj.ax_3d, 0.55 * obj.aircraft_dimensions * 1.5, 0, 1 * obj.aircraft_dimensions * 1.5, ...
+            obj.Animation.time_text_handle = text(obj.Animation.PlotAxes.ax_3d, 0.55 * obj.Model3D.aircraft_dimensions * 1.5, 0, 1 * obj.Model3D.aircraft_dimensions * 1.5, ...
                 't = 0 sec',...
                 'FontSize', 20);
         end
         
         function obj = plot_position(obj, pos)
-            obj.PosPlotHandle = plot3(obj.ax_pos, pos(1,1), pos(1,2), pos(1,3), ...
+            obj.Animation.PosPlotHandle = plot3(obj.Animation.PlotAxes.ax_pos, pos(1,1), pos(1,2), pos(1,3), ...
             'XDataSource', 'pos_n', 'YDataSource', 'pos_w', 'ZDataSource', 'pos_h');
-            axis(obj.ax_pos, 'equal');
-            max_pos = max([abs(pos); [50 50 50]]);
-            viewbox = [-max_pos(1) max_pos(1) -max_pos(2) max_pos(2) 0 max_pos(3)] * 1.2;
-            axis(obj.ax_pos, viewbox);
+            axis(obj.Animation.PlotAxes.ax_pos, 'equal');
+            mAnimation.PlotAxes.ax_pos = max([abs(pos); [50 50 50]]);
+            viewbox = [-mAnimation.PlotAxes.ax_pos(1) mAnimation.PlotAxes.ax_pos(1) -mAnimation.PlotAxes.ax_pos(2) mAnimation.PlotAxes.ax_pos(2) 0 mAnimation.PlotAxes.ax_pos(3)] * 1.2;
+            axis(obj.Animation.PlotAxes.ax_pos, viewbox);
             set(gcf,'Color',[1 1 1])
-            view(obj.ax_pos, [30 10])
-            grid(obj.ax_pos, 'on');
+            view(obj.Animation.PlotAxes.ax_pos, [30 10])
+            grid(obj.Animation.PlotAxes.ax_pos, 'on');
             
-            title(obj.ax_pos, 'Position', 'FontSize', 16)
-            xlabel(obj.ax_pos, "north [m]");
-            ylabel(obj.ax_pos, "west [m]");
-            zlabel(obj.ax_pos, "altitude [m]");
+            title(obj.Animation.PlotAxes.ax_pos, 'Position', 'FontSize', 16)
+            xlabel(obj.Animation.PlotAxes.ax_pos, "north [m]");
+            ylabel(obj.Animation.PlotAxes.ax_pos, "west [m]");
+            zlabel(obj.Animation.PlotAxes.ax_pos, "altitude [m]");
         end
         
         function plot_trajectory(obj, t_trajectory, x_trajectory)
@@ -173,7 +171,9 @@ classdef AircraftVisualizer
             psi = x(:,12);
             
             obj.plot_aircraft();
-            obj = obj.plot_position(pos);
+            if obj.Animation.show_position_plot
+                obj = obj.plot_position(pos);
+            end
             obj = obj.plot_inputs(t, x);
             obj = obj.plot_text();
            
@@ -183,57 +183,66 @@ classdef AircraftVisualizer
                 Mx = makehgtform('xrotate', phi(i));
                 My = makehgtform('yrotate', -theta(i));
                 Mz = makehgtform('zrotate', -psi(i));
-                set(obj.aircraft_transformation, 'Matrix', Mx*My*Mz);
+                set(obj.Animation.aircraft_transformation, 'Matrix', Mx*My*Mz);
                 
                 % Update text
-                set(obj.TextHandles.time_text_hdl, 'String', sprintf('t = %3.2f sec',t(i)))
+                set(obj.Animation.time_text_handle, 'String', sprintf('t = %3.2f sec',t(i)))
 
-                % Update position plot
-                pos_n = pos(1:i,1);
-                pos_w = pos(1:i,2);
-                pos_h = pos(1:i,3);
-                refreshdata(obj.PosPlotHandle, 'caller');
+                if obj.Animation.show_position_plot
+                    % Update position plot
+                    pos_n = pos(1:i,1);
+                    pos_w = pos(1:i,2);
+                    pos_h = pos(1:i,3);
+                    refreshdata(obj.Animation.PosPlotHandle, 'caller');
+                end
                 
                 % Update input plots
                 time = t(1:i);
                 delta_a_evolution = rad2deg(x(1:i,13));
                 delta_e_evolution = rad2deg(x(1:i,14));
                 delta_r_evolution = rad2deg(x(1:i,15));
-                refreshdata(obj.InputPlotHandles{1}, 'caller');
-                refreshdata(obj.InputPlotHandles{2}, 'caller');
-                refreshdata(obj.InputPlotHandles{3}, 'caller');
+                refreshdata(obj.Animation.InputPlotHandles{1}, 'caller');
+                refreshdata(obj.Animation.InputPlotHandles{2}, 'caller');
+                refreshdata(obj.Animation.InputPlotHandles{3}, 'caller');
                 
                 % Control the animation speed
-                if obj.timestep * i - toc > 0
-                    pause(max(0, obj.timestep * i - toc))
+                if obj.Animation.timestep * i - toc > 0
+                    pause(max(0, obj.Animation.timestep * i - toc))
                 end
             end
         end
         
         function obj = create_ax_object(obj)
-            obj.fig = figure;
+            obj.Animation.fig = figure;
             screensize = get(0,'ScreenSize');
+            
+            % Show position plot and move others plot
+            if obj.Animation.show_position_plot
+                obj.Animation.PlotAxes.ax_pos = axes(obj.Animation.fig, 'position',[0.05 0.5 0.2 0.4]);
+                
+                pos_plot_position = [0.25 0.0 0.5 1];
+                input_plot_positions = [0.75 0.7 0.2 0.15;
+                                        0.75 0.5 0.2 0.15;
+                                        0.75 0.3 0.2 0.15];
+            else
+                pos_plot_position = [0 0.0 0.5 1];
+                input_plot_positions = [0.6 0.7 0.35 0.15;
+                                        0.6 0.5 0.35 0.15;
+                                        0.6 0.3 0.35 0.15];
+            end
+            
             set(gcf,'Position', ...
-                [screensize(3)/40 screensize(4)/12 screensize(3)/2*2 screensize(3)/2.2*1.0],...
+                [screensize(3)/40 screensize(4)/12 screensize(3)/1.5 screensize(4)/1.5],...
                 'Visible','on');
             
-            % 3D position
-            obj.ax_pos = axes(obj.fig, 'position',[0.05 0.5 0.2 0.4]);
-            
-            % 3D animation
-            obj.ax_3d = axes(obj.fig, 'position',[0.25 0.0 0.5 1]);
+            obj.Animation.PlotAxes.ax_3d = axes(obj.Animation.fig, 'position', pos_plot_position);
             axis off
-            set(obj.ax_3d,'color','none');
-            axis(obj.ax_3d, 'equal')
+            set(obj.Animation.PlotAxes.ax_3d,'color','none');
+            axis(obj.Animation.PlotAxes.ax_3d, 'equal')
             hold on;
-            
-            % Aircraft inputs
-            input_plot_positions = [0.75 0.7 0.2 0.15;
-                                    0.75 0.5 0.2 0.15;
-                                    0.75 0.3 0.2 0.15];
                                     
             for i = 1:3
-                obj.ax_inputs{i} = axes(obj.fig, 'position', input_plot_positions(i,:)); 
+                obj.Animation.PlotAxes.ax_inputs{i} = axes(obj.Animation.fig, 'position', input_plot_positions(i,:)); 
             end
         end
     end
